12649990db020ae2ea0aac5d0797caa28d05e17c
jnRosa22 <juanlemos67@gmail.com>
2022-02-27 19:26:58 -0300

    otimização

--- forca.cpp
+++ forca.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <string>
 using namespace std;
+string word,kword;
+char guess;
+int len,life=3,w=0;
+
+
+
+void analyze_word();
+void analyze_char();
 
 int main(){
-    //variable system
-    string word,kword;
-    char guess;
-    int len,life=3,w=0;
-    bool right=false;
-
     // word that player will guess
-    cout<<"whire one word for to start the game: ";
+    cout<<"write a word to start the game: ";
     cin>>word;
-    for(int j,i=0;i<30;i++){
+    analyze_word();
+    while(life>0){
+        cout<<"lifes: "<<life<<'\n';
+        cout<<kword<<"\n"<<"guess one char :";
+        cin>>guess;
+        analyze_char();
+        if (kword==word){
+            cout <<"\nyou won!! the word was "<<word<<"\n";
+            break;
+        }
+
+        //reset system
+        w=0;
+        system("cls");
+
+    }
+    if(life==0){
+        cout<<"\nyou lose!!! the word was "<< word<<"\n";
+    }
+    system("pause");
+}
+void analyze_word(){
+        for(int j,i=0;i<30;i++){
         if (word[i]!='\0'){
             continue;
         }else{
@@ -24,35 +48,18 @@ int main(){
             break;   
         }
     }
-    while(life>0){
-        cout<<"lifes: "<<life<<'\n';
-        cout<<kword<<"\n"<<"guess one char :";
-        cin>>guess;
-        while(w<=len){
-            if (word[w]==guess){
-                kword[w]=guess;
-                right=true;
-                cout << guess <<"\n";
-                system("pause");
-            }
-            if (w==len && right==false){
-                life-=1;
-            }
+}
+void analyze_char(){
+    bool right;
+    while(w<=len){
+        if (word[w]==guess){
+            kword[w]=guess;
+            right=true;
+        }
+        if (w==len && right==false){
+            life-=1;
+        }
             w++;
-        }
-        if (kword==word){
-            cout <<"you won!! the word was "<<word<<"\n";
-            break;
-        }
-
-        //reset system
-        w=0;
-        right=false;
-        system("cls");
-
     }
-    if(life==0){
-        cout<<"you lose!!! the word was "<< word<<"\n";
-    }
-    system("pause");
+    right=false;
 }
